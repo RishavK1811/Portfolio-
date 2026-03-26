64f59be32c6577ee2b5c151969d47cf841cb173f
RishavK1811 <rishavkumar181101@gmail.com>
2026-03-26 07:18:46 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,46 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
+window.onload = function () {
 
-    // EmailJS init
-    emailjs.init("9RELraKgnALzG9l1o");
+    emailjs.init("9RELRaKgnALzG9l1o");
 
-    // Popup function
-    function showPopup(message, isError = false) {
-        const popup = document.getElementById("popup");
-        popup.innerText = message;
-
-        popup.classList.remove("error");
-        if (isError) popup.classList.add("error");
-
-        popup.classList.add("show");
-
-        setTimeout(() => {
-            popup.classList.remove("show");
-        }, 3000);
-    }
-
-    // Form select
     const form = document.querySelector("form");
 
     form.addEventListener("submit", function (e) {
         e.preventDefault();
 
-        const templateParams = {
-            from_name: document.getElementById("name").value,
-            from_email: document.getElementById("email").value,
-            subject: document.getElementById("subject").value,
-            message: document.getElementById("message").value,
-            to_email: "rishavkumar181101@gmail.com"
-        };
-
-        emailjs.send("service_kzs7fpx", "template_dkhpyrp", templateParams)
+        emailjs.sendForm("service_kzs7fpx", "template_dkhpyrp", this)
             .then(function () {
-                showPopup("Message sent successfully ✅");
-                form.reset();
+                alert("Message sent successfully ✅");
             })
             .catch(function (error) {
-                showPopup("Failed to send ❌", true);
+                alert("Failed ❌");
                 console.log(error);
             });
+
+        form.reset();
     });
 
-});
+};
